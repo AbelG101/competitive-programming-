e6c37f66405f8686e61fc94c20867355c65c1abc
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2024-02-06 20:39:10 +0300

    Create Mod_Power[HR].py

---
+++ Mod_Power[HR].py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT   
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+power_result = a**b
+modulo_result = power_result % c
+print(power_result)
+print(modulo_result)
