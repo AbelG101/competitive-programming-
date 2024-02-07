41630fb08f4988953ef10a4f4b32c74bcfd245d9
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2024-02-07 15:33:23 +0300

    Update runner-up_score.py

--- runner-up_score.py
+++ runner-up_score.py
@@ -1,11 +1,13 @@
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    
+
     arr.sort()
-    
+
+    # set max score to the last item of sorted array which is the largest value
     max_score = arr[-1]
-    
+
+    # iterate in reverse & find the 2nd smallest value
     for i in range(len(arr)-1, -1, -1):
         if arr[i] < max_score:
             print(arr[i])
