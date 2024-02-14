9d1d16aab518d66cb6644713a8bc08d8cbcc7710
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2024-02-15 00:04:13 +0300

    Time: 657 ms (5.41%), Space: 12.1 MB (40.54%) - LeetHub

--- 0822-card-flipping-game/0822-card-flipping-game.py
+++ 0822-card-flipping-game/0822-card-flipping-game.py
@@ -5,16 +5,9 @@ class Solution(object):
         :type backs: List[int]
         :rtype: int
         """
-        my_set = set(fronts + backs)
-        def is_good(current_card):
-            for front_card, back_card in zip(fronts, backs):
-                if front_card == back_card == current_card:
-                    return False
-            return True
-
-        for card in sorted(my_set):
-            if is_good(card):
+        unique_cards = set(fronts + backs)
+        for card in sorted(unique_cards):
+            if all(front != card or back != card for front, back in zip(fronts, backs)):
                 return card
-
         return 0
         
