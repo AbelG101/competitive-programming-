2ee07c2f54f9d803f19fbc2e8fdfe523eabc4970
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2024-02-23 19:14:31 +0300

    Time: 37 ms (97.21%), Space: 12.3 MB (100.00%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -7,8 +7,10 @@ class Solution(object):
         rows = len(matrix)
         cols = len(matrix[0])
 
+        # result matrice size should be cols x rows not the other way around
         result = [[0]*rows for i in range(cols)]
 
+        # since we're transponsing loop should start from cols 
         for col in range(cols):
             for row in range(rows):
                 result[col][row] = matrix[row][col]
